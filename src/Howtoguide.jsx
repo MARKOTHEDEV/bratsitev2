42484b9969797405143cb3ba4e6a114836a94e38
markothedev <markothedevmail@gmail.com>
2024-09-20 19:51:27 +0100

    added typeform

--- src/Howtoguide.jsx
+++ src/Howtoguide.jsx
@@ -9,7 +9,7 @@ const Howtoguide = () => {
   return (
     <div className='bg-black px-10 md:px-20 flex flex-col md:flex-row py-24 justify-center  items-center border-t-4 border-[#9fd830]'>
     <div className='flex flex-col items-center gap-4'>
-      <a target='_blank' href="https://x.com/intent/post?text=This+is+proof+of+my+entry+in+%40BratCoinCTO+Early+Access+and+5+SOL+Giveaway.%0A%0ALaunching+Soon.%0A%0ADrop+Sol+Wallet+On+Pinned+Post+and+Join+TG+Now%21%0A%0ABe+Early+For+Once.+%F0%9F%90%B0%0A%0Apoap.brat.meme">
+      <a target='_blank' href="https://form.typeform.com/to/RrT95hZu">
       <img src={poap2} className='block w-[600px]' alt="poap" /></a>
       <p className='font-bold text-white'>Click POAP to sign up for Early Access + Giveaway</p>
     </div>
